bf1afd3427310f522365842fc11bd388746453a0
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-26 19:48:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,9 +2,6 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  #html = File.read('fixtures/kickstarter.html')
-  k#ickstarter = Nokogiri::HTML(html)
-  
   projects = {}
   
   kickstarter.css("li.project.grid_4").each do |project|
